0a440a66f6877475954839920bc5e03cc2a3de53
terry tang <thstb@foxmail.com>
2019-05-07 16:31:03 +0800

    view dependent

--- src/com/bn/CLODUtil.java
+++ src/com/bn/CLODUtil.java
@@ -8,7 +8,7 @@ public class CLODUtil
 	HDTData hdtd;
 	int maxLevel;//最大树层数，从1开始数
 	int[][] treeData;
-	int[][] DHMatrix;
+	int[][] DHMatrix;//存储顶点dh的矩阵
 	
 	List<int[]> mesh=new ArrayList<int[]>();
 		
@@ -29,12 +29,6 @@ public class CLODUtil
 		
 		modifyDHMatrix();
 		
-//		for(int i=0;i<hdtd.width;i++){
-//			for(int j=0;j<hdtd.width;j++){
-//				System.out.print(""+DHMatrix[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
 		//计算出第1层的中心点 行列号
 		int center=(hdtd.width)/((int)(Math.pow(2, 1)));	
 		traversal(center,center,hdtd.width);
@@ -49,7 +43,7 @@ public class CLODUtil
 	public int getDHMatrix(int x,int z){
 		return DHMatrix[x][z];
 	}
-	public void modifyDHMatrix(){
+	public void modifyDHMatrix(){//计算dh  从最小的网格开始  向上计算dh值  保证相邻的网格拆分的程度差别不会大于1
 		int edgeLength = 2;
 		while(edgeLength<=hdtd.width){
 			int halfEdgeLength=edgeLength>>1; 
@@ -57,7 +51,7 @@ public class CLODUtil
 			//System.out.println("halfEdgeLength"+halfEdgeLength+" halfChildEdgeLength:"+halfChildEdgeLength);
 			for(int z=halfEdgeLength;z<hdtd.height;z+=edgeLength){  
 				for(int x=halfEdgeLength;x<hdtd.width;x+=edgeLength)  
-					if(edgeLength==2){  
+					if(edgeLength==2){  		//最小的网格
 						int[] DH6=new int[6];  
 						DH6[0] = Math.abs(((getRawHeight(x-halfEdgeLength,z+halfEdgeLength)+getRawHeight(x+halfEdgeLength,z+halfEdgeLength))>>1)-getRawHeight(x,z+halfEdgeLength));  
 						DH6[1] = Math.abs(((getRawHeight(x+halfEdgeLength,z+halfEdgeLength)+getRawHeight(x+halfEdgeLength,z-halfEdgeLength))>>1)-getRawHeight(x+halfEdgeLength,z));  
@@ -71,7 +65,7 @@ public class CLODUtil
 								DHMax=DH6[i];  
 						}  
 						setDHMatrix(x,z,DHMax);  
-					} else {  
+					} else {  				//不是最小的网格时  那就需要查看相邻网格的dh情况 取最大值
 						int DH14[] = new int[14];  
 						int numDH=0;
 						
@@ -127,7 +121,7 @@ public class CLODUtil
 						DH14[numDH]=Math.abs(((getRawHeight(x+halfEdgeLength,z-halfEdgeLength)+getRawHeight(x-halfEdgeLength,z+halfEdgeLength))>>1)-getRawHeight(x,z));  
 						numDH++;  
 						int DHMax=DH14[0];  
-						for(int i=0;i<14;i++){  
+						for(int i=0;i<14;i++){  //取最大的dh
 							if(DHMax<DH14[i])  
 								DHMax=DH14[i];  
 						}  
@@ -138,53 +132,6 @@ public class CLODUtil
 		}
 	}
 	
-	
-	//判断一个矩形是否需要切分的方法
-	//计算当前矩形的粗糙度
-	// 0-1-2
-	// |\|/|
-	// 3-4-5
-	// |/|\|
-	// 6-7-8
-	public boolean needQF(int col,int row,int lp)
-	{
-		int[][] offset={{-1,-1},{0,-1},{1,-1}, {-1,0},{0,0},{1,0}, {-1,1},{0,1},{1,1},};
-		//取出1~9号点的灰度值
-		int[] hd=new int[9];
-		for(int i=0;i<9;i++)
-		{
-			hd[i]=hdtd.data[col+offset[i][0]*lp][row+offset[i][1]*lp];
-		}
-		
-		//计算中心点到上下左右四个点的灰度差以及对角线的四个点的灰度差
-		int[] dh=new int[6];		
-        dh[0]=Math.abs(hd[4]-hd[1]);
-        dh[1]=Math.abs(hd[4]-hd[3]);
-        dh[2]=Math.abs(hd[4]-hd[5]);
-        dh[3]=Math.abs(hd[4]-hd[7]);
-        dh[4]=Math.abs(hd[0]-hd[8]);
-        dh[5]=Math.abs(hd[6]-hd[2]);
-        
-        //找到最大的高度差
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<6;i++)
-        {
-        	if(max<dh[i])
-        	{
-        		max=dh[i];
-        	}
-        }
-        
-        //若最大高度差大于指定阈值
-        if(max>thold)
-        {
-        	return true;
-        }
-        else
-        {
-        	return false;
-        }	
-	}
 	
 	//遍历计算是否需要拆分的递归方法(返回值为true表示需要细分，为false表示不需要细分)
 	public void traversal(int col,int row, int edgeLength)
@@ -197,33 +144,36 @@ public class CLODUtil
 		}		
 
 	    //如果自己的四个孩子都不需要切分，看看自己需不需要拆分
-		//boolean flag = needQF(col,row,lp);
-	    float fViewDistance=10,f;
+	    float fViewDistance,f;
 		int halfChildEdgeLength;
 		int childEdgeLength;
 		boolean blend;
 
-		//fViewDistance=frustum.distanceOfTwoPoints(centerQuad);
-		int mfMinResolution = thold;
-		int mfDetailLevel = 1;
-		f = fViewDistance/(edgeLength*mfMinResolution*(Math.max(mfDetailLevel*getDHMatrix(col,row),1.0f)));
-	
+		//以下为考虑了与摄像机的距离作为是否拆分的计算方法
+		fViewDistance=1000;//frustum.distanceOfTwoPoints(centerQuad);  //计算与摄像机的距离
+		int mfMinResolution = 4;
+		int mfDetailLevel =6;
+		f = fViewDistance/(edgeLength*mfMinResolution*mfDetailLevel*(Math.max(getDHMatrix(col,row),1.0f)));
+		
 		if(f<1.0f){
 			blend=true;
 			treeData[col][row]=1;
 		}else{
 			blend=false;
+			//System.out.println("f:"+f);
 			treeData[col][row]=0;
 		}
 		
-		if(getDHMatrix(col,row)>20){
-			blend=true;
-			treeData[col][row]=1;
-		}else{
-			blend=false;
-			treeData[col][row]=0;
-		}
-		//System.out.println(""+col+" "+row+" flag:"+treeData[col][row]+" edgeLength:"+treeData[col][row]);
+		
+		//此时判断是否拆分 只考虑了某点的粗糙度 超过阈值才需要拆分
+//		if(getDHMatrix(col,row)>20){
+//			blend=true;
+//			treeData[col][row]=1;
+//		}else{
+//			blend=false;
+//			treeData[col][row]=0;
+//		}
+
 		
 		if(blend){
 			halfChildEdgeLength 	= edgeLength>>2;
@@ -243,9 +193,16 @@ public class CLODUtil
 		//取出当前节点标志值
 		int flag=treeData[col][row];
 
-		//若为无效节点则返回
+		//若为无效节点则生成
 		if(flag==9){
-			System.out.println("col:"+col+" row:"+row+" "+getDHMatrix(col,row)+" "+flag);
+			//若为叶子节点则生成数据
+			// 0-1-2
+			// |\|/|
+			// 3-4-5
+			// |/|\|
+			// 6-7-8
+			
+			//System.out.println("col:"+col+" row:"+row+" "+getDHMatrix(col,row)+" "+flag);
 			return;
 		}
 		//若为叶子节点则生成数据
@@ -255,19 +212,24 @@ public class CLODUtil
 		// |/|\|
 		// 6-7-8
 		//计算出当前层的跨度
-	    int lp=(hdtd.width)/((int)(Math.pow(2, currLevel)));	
-	    int up = lp<<1;
+	    int lp=(hdtd.width)/((int)(Math.pow(2, currLevel)));
+	    //当前层跨度的两倍
+	    int up = lp<<1; 
+	    
 	    int neighborX;  
 		int neighborZ;  
-		if(flag==0)
+		if(flag==0)//不需要拆分
 		{		
 			//0-1
 			mesh.add(new int[]{col-lp,row-lp,col,row-lp});
 			//1-2
 			mesh.add(new int[]{col,row-lp,col+lp,row-lp});
 			
-			
-			
+			// 							0-1-2
+			// 							|\|/|
+			// 查看这个点是否拆分 	 *- 3-4-5 
+			// 							|/|\|
+			// 							6-7-8
 			neighborX = col-up;  
 			neighborZ = row;  
 			if(neighborX<=0||(treeData[neighborX][neighborZ]!=9)){  
@@ -276,6 +238,12 @@ public class CLODUtil
 			}
 			
 			
+
+			// 0-1-2
+			// |\|/|
+			// 3-4-5-*   查看这个点是否拆分
+			// |/|\|
+			// 6-7-8
 			neighborX = col+up;  
 			neighborZ = row;  
 			if(neighborX>=hdtd.width||(treeData[neighborX][neighborZ]!=9)){  
@@ -293,6 +261,14 @@ public class CLODUtil
 			//3-6
 			mesh.add(new int[]{col-lp,row,col-lp,row+lp});
 			
+			
+			//   *      查看这个点是否拆分
+			//   |
+			// 0-1-2
+			// |\|/|
+			// 3-4-5
+			// |/|\|
+			// 6-7-8
 			neighborX = col;  
 			neighborZ = row-up;  
 			if(neighborZ<=0||(treeData[neighborX][neighborZ]!=9)){  
@@ -300,6 +276,14 @@ public class CLODUtil
 				mesh.add(new int[]{col,row-lp,col,row});
 			}
 			
+
+			// 0-1-2
+			// |\|/|
+			// 3-4-5
+			// |/|\|
+			// 6-7-8
+			//   |
+			//   *      查看这个点是否拆分
 			neighborX = col;  
 			neighborZ = row+up;  
 			if(neighborZ>=hdtd.width||(treeData[neighborX][neighborZ]!=9)){  
@@ -326,7 +310,7 @@ public class CLODUtil
 		//若为非叶子节点
 		//计算下一层的跨度
 		int lpn=(hdtd.width-1)/((int)(Math.pow(2, currLevel+1)));	
-		if(flag==1)
+		if(flag==1)//需要拆分
 		{
 			//先拿到四个孩子的标志  左上  右上  左下  右下
 		    int ul=treeData[col-lpn][row-lpn];
@@ -334,82 +318,81 @@ public class CLODUtil
 		    int ll=treeData[col-lpn][row+lpn];
 		    int lr=treeData[col+lpn][row+lpn];
 		    
-		    
 		    //如果左上角孩子不是无效节点去孩子
-//		    if(ul!=9)
-//		    {
+		    if(ul!=9)
+		    {
 		    	traversalGenMesh(col-lpn,row-lpn, currLevel+1);
-//		    }
-//		    else
-//		    {//如果孩子是无效节点
-//		    	//0-1
-//				mesh.add(new int[]{col-lp,row-lp,col,row-lp});
-//				//0-3
-//				mesh.add(new int[]{col-lp,row-lp,col-lp,row});
-//				//3-4
-//				mesh.add(new int[]{col-lp,row,col,row});
-//				//1-4
-//				mesh.add(new int[]{col,row-lp,col,row});
-//				//0-4
-//				mesh.add(new int[]{col-lp,row-lp,col,row});
-//		    }
-//		    
-//		    //如果右上角孩子不是无效节点去孩子
-//		    if(ur!=9)
-//		    {
+		    }
+		    else
+		    {//如果孩子是无效节点
+		    	//0-1
+				mesh.add(new int[]{col-lp,row-lp,col,row-lp});
+				//0-3
+				mesh.add(new int[]{col-lp,row-lp,col-lp,row});
+				//3-4
+				mesh.add(new int[]{col-lp,row,col,row});
+				//1-4
+				mesh.add(new int[]{col,row-lp,col,row});
+				//0-4
+				mesh.add(new int[]{col-lp,row-lp,col,row});
+		    }
+		    
+		    //如果右上角孩子不是无效节点去孩子
+		    if(ur!=9)
+		    {
 		    	traversalGenMesh(col+lpn,row-lpn, currLevel+1);
-//		    }
-//		    else
-//		    {//如果孩子是无效节点
-//		    	//1-2
-//				mesh.add(new int[]{col,row-lp,col+lp,row-lp});
-//				//1-4
-//				mesh.add(new int[]{col,row-lp,col,row});
-//				//4-5
-//				mesh.add(new int[]{col,row,col+lp,row});
-//				//2-5
-//				mesh.add(new int[]{col+lp,row-lp,col+lp,row});
-//				//2-4
-//				mesh.add(new int[]{col+lp,row-lp,col,row});
-//		    }
-//		    
-//		    //如果左下角孩子不是无效节点去孩子
-//		    if(ll!=9)
-//		    {
+		    }
+		    else
+		    {//如果孩子是无效节点
+		    	//1-2
+				mesh.add(new int[]{col,row-lp,col+lp,row-lp});
+				//1-4
+				mesh.add(new int[]{col,row-lp,col,row});
+				//4-5
+				mesh.add(new int[]{col,row,col+lp,row});
+				//2-5
+				mesh.add(new int[]{col+lp,row-lp,col+lp,row});
+				//2-4
+				mesh.add(new int[]{col+lp,row-lp,col,row});
+		    }
+		    
+		    //如果左下角孩子不是无效节点去孩子
+		    if(ll!=9)
+		    {
 		    	traversalGenMesh(col-lpn,row+lpn, currLevel+1);
-//		    }
-//		    else
-//		    {//如果孩子是无效节点
-//		    	//3-6
-//				mesh.add(new int[]{col-lp,row,col-lp,row+lp});
-//				//3-4
-//				mesh.add(new int[]{col-lp,row,col,row});
-//				//4-7
-//				mesh.add(new int[]{col,row,col,row+lp});
-//				//6-7
-//				mesh.add(new int[]{col-lp,row+lp,col,row+lp});
-//				//4-6
-//				mesh.add(new int[]{col,row,col-lp,row+lp});	
-//		    }
-//		    
-//		    //如果右下角孩子不是无效节点去孩子
-//		    if(lr!=9)
-//		    {
+		    }
+		    else
+		    {//如果孩子是无效节点
+		    	//3-6
+				mesh.add(new int[]{col-lp,row,col-lp,row+lp});
+				//3-4
+				mesh.add(new int[]{col-lp,row,col,row});
+				//4-7
+				mesh.add(new int[]{col,row,col,row+lp});
+				//6-7
+				mesh.add(new int[]{col-lp,row+lp,col,row+lp});
+				//4-6
+				mesh.add(new int[]{col,row,col-lp,row+lp});	
+		    }
+		    
+		    //如果右下角孩子不是无效节点去孩子
+		    if(lr!=9)
+		    {
 		    	traversalGenMesh(col+lpn,row+lpn, currLevel+1);
-//		    }
-//		    else
-//		    {//如果孩子是无效节点
-//		    	//4-5
-//				mesh.add(new int[]{col,row,col+lp,row});
-//				//4-7
-//				mesh.add(new int[]{col,row,col,row+lp});
-//				//5-8
-//				mesh.add(new int[]{col+lp,row,col+lp,row+lp});
-//				//7-8
-//				mesh.add(new int[]{col,row+lp,col+lp,row+lp});
-//				//4-8
-//				mesh.add(new int[]{col,row,col+lp,row+lp});
-//		    }
+		    }
+		    else
+		    {//如果孩子是无效节点
+		    	//4-5
+				mesh.add(new int[]{col,row,col+lp,row});
+				//4-7
+				mesh.add(new int[]{col,row,col,row+lp});
+				//5-8
+				mesh.add(new int[]{col+lp,row,col+lp,row+lp});
+				//7-8
+				mesh.add(new int[]{col,row+lp,col+lp,row+lp});
+				//4-8
+				mesh.add(new int[]{col,row,col+lp,row+lp});
+		    }
 		}		
 	}
 }
